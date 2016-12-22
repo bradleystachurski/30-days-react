532e3f99c2447dce2012daf92e7ac268efd8a2bb
Bradley Stachurski <bradley.stachurski@gmail.com>
2016-12-21 21:35:18 -0700

    Finished Redux tutorial

--- NewBoston/React-Redux-Boilerplate/dev/js/components/App.js
+++ NewBoston/React-Redux-Boilerplate/dev/js/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
+import UserDetail from '../containers/user-detail';
 require('../../scss/style.scss');
 
 const App = () => (
     <div>
-        <h2>User List</h2>
+        <h2>Username List:</h2>
         <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
+        <hr/>
+        <h2>User Details:</h2>
+        <UserDetail />
     </div>
 );
 
